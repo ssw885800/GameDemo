af22fe10f657cdb53274920a7e1d3c0959df6b0c
ssw885800 <a0987349590a@gmail.com>
2023-04-15 12:36:33 +0800

    FirstUpload
    見光死(暫定名稱)遊戲專案
    初次上傳
    Version:0.0.1

---
+++ 20230413GameDemo/Assets/Test/TestScript/TestPlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestPlayerController : MonoBehaviour
+{
+    
+    public Rigidbody PlayerRigidbody;
+
+
+    [Header("移動參數")]
+    [Range(5f,100f)]public float MoveSpeed;
+    float MoveDir;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void Move()     //移動
+    {
+        MoveDir = Input.GetAxis("Horizontal");  //取得左右移動按鍵
+        PlayerRigidbody.velocity = new Vector2(MoveDir * MoveSpeed , PlayerRigidbody.velocity.y);      //一般移速
+    }
+   
+}
