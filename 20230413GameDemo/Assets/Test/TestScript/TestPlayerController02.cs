908788c47d03a96046197d3b1c9e3eafdddb3dec
ssw885800 <a0987349590a@gmail.com>
2023-04-26 20:13:56 +0800

    第三人稱移動練習
    新增地面檢測、跳躍、角色控制器與場景中鋼體互動

--- 20230413GameDemo/Assets/Test/TestScript/TestPlayerController02.cs
+++ 20230413GameDemo/Assets/Test/TestScript/TestPlayerController02.cs
@@ -11,6 +11,14 @@ public class TestPlayerController02 : MonoBehaviour
     public float Gravity = -9.8f;   //重力
     private Vector3 Velocity = Vector3.zero;
 
+    [Header("跳躍參數")]
+    public float JumpHeight = 3f;
+
+    [Header("地面檢測")]
+    public Transform GroundCheck;   //獲取地面檢測體
+    public float CheckRadius = 0.2f;//檢測範圍
+    private bool IsGround;          //是否在地上
+    public LayerMask layerMask;     //獲取層級遮罩
     // Start is called before the first frame update
     void Start()
     {
@@ -23,8 +31,17 @@ public class TestPlayerController02 : MonoBehaviour
         Move01();
         //Move02();
     }
-    void Move01()//第三人稱移動方式
+    void Move01()//第三人稱移動方式，w、s控制前後;a、d控制方向(角度)
     {
+        IsGround = Physics.CheckSphere(GroundCheck.position, CheckRadius,layerMask);
+        if (IsGround && Velocity.y < 0)
+        {
+            Velocity.y = 0;
+        }
+        if (IsGround && Input.GetButtonDown("Jump"))//跳躍
+        {
+            Velocity.y += Mathf.Sqrt(JumpHeight * -2 * Gravity);
+        }
         var horizontal = Input.GetAxis("Horizontal");   //獲取InputManager的水平輸入
         var vertical = Input.GetAxis("Vertical");       //獲取InputManager的垂直輸入
 
@@ -51,6 +68,15 @@ public class TestPlayerController02 : MonoBehaviour
 
         transform.eulerAngles = new Vector3(transform.eulerAngles.x, angle, transform.eulerAngles.z);
     }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)//角色控制器與鋼體互動的方法
+    {
+        if (hit.transform.CompareTag("Finish"))
+        {
+            hit.rigidbody.AddForce(transform.forward * MoveSpeed);
+        }
+    }
+
 
 }
 
