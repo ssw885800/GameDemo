8262fd09f2a3bf5508c5e8613ab4a501553a185a
ssw885800 <a0987349590a@gmail.com>
2023-05-10 03:28:03 +0800

    New input system test
    20230510

---
+++ 20230413GameDemo/Assets/Test/TestScript/TestPlayerController03.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
+
+public class TestPlayerController03 : MonoBehaviour
+{
+    private CharacterController Controller;//獲取物件上的角色控制器
+
+    [Header("移動參數")]
+    [Range(5f, 100f)] public float MoveSpeed;//移動速度
+    [Range(5f, 20f)] public float JumpHigh;//跳躍高度
+    public float Gravity = -9.8f;   //重力
+    private Vector3 Velocity = Vector3.zero;    //3維向量變數
+    private Vector2 movement;
+
+    [Header("地面檢測")]
+    public Transform GroundCheck;   //獲取地面檢測體
+    public float CheckRadius = 0.2f;//檢測範圍
+    private bool IsGround;          //是否在地上
+    public LayerMask layerMask;     //獲取層級遮罩
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Controller = transform.GetComponent<CharacterController>(); //獲取玩家身上的控制器組件
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GravityAdd();
+        PlayerMove();
+    }
+    public void PlayerMoveInput(InputAction.CallbackContext callbackContext)
+    {
+         movement = callbackContext.ReadValue<Vector2>();
+    }
+    void PlayerMove()
+    {
+        IsGround = Physics.CheckSphere(GroundCheck.position, CheckRadius, layerMask);
+
+        var move = transform.forward * MoveSpeed * movement.x * Time.deltaTime;
+        Controller.Move(move);
+    }
+    void GravityAdd()
+    {
+        if (IsGround && Velocity.y < 0)                 //判斷是否在地面，如果是，將落下的動量歸零
+        {
+            Velocity.y = 0;
+        }
+        Velocity.y += Gravity * Time.deltaTime;     //重力加速度
+        Controller.Move(Velocity * Time.deltaTime);
+    }
+}
